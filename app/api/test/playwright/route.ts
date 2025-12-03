2859a8af65f60f492b187b3b1f014c39d9ccf9e6
crymorebaby <crymoreaniri@gmail.com>
2025-12-04 00:26:06 +0300

    upd

--- app/api/test/playwright/route.ts
+++ app/api/test/playwright/route.ts
@@ -2,6 +2,8 @@ import { join } from "path"
 import { readFile } from "fs/promises"
 import { NextResponse } from "next/server"
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
     const htmlReportPath = join(process.cwd(), 'playwright-report', 'index.html')
     const htmlContent = await readFile(htmlReportPath, 'utf-8')
